f00ee68b7464c7efee7433ae8427f77808fdaa2e
Keywind <watersprayer127@gmail.com>
2024-03-15 11:37:47 +0800

    [ FIX ] [ 2024/03/15 ] [ Add Main.py ]

---
+++ ip_address_file_finder/__main__.py
@@ -0,0 +1,11 @@
+import argparse
+
+from main import main
+
+parser = argparse.ArgumentParser(description = "This module is used to find all IP address and port numbers of files.")
+
+parser.add_argument("-d", "--directory", help = "Directory to check for IP and PORT.", default = "./")
+
+args = parser.parse_args()
+
+main(args.directory)
