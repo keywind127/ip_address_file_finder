d13ba739d71bb494699c4eeb4362c6a35a189541
Keywind <watersprayer127@gmail.com>
2024-03-15 11:33:50 +0800

    [ DOCS ] [ 2024/03/15 ] [ Added Setup ]

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "ip-address-file-finder",
+    version = '0.0.1',
+    description = 'This module is used to find files containing IP addresses and PORTs.',
+    author = 'keywind127',
+    author_email = 'watersprayer127@gmail.com',
+    url = 'https://github.com/keywind127/ip_address_file_finder',
+    packages = [ "ip_address_file_finder" ],
+    install_requires = [
+        'Markdown',
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
